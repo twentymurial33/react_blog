5197250de3f753d5e318c06cb4f9985140811c81
Murial Anindo <murial.anindo@lonett.com>
2022-05-26 08:30:27 -0400

    adding components and actually building on layout componnent nicely

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Link from "./Link";
+import Main from "./MainContent";
+
+function Layout() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default Layout;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,6 @@ import "../style.css";
 function Nav() {
   return (
     <div className="Navbar">
-      <img src="../images" />
       <h3>Murial's React Journey</h3>
       <h4>All My React Tutorials</h4>
     </div>
