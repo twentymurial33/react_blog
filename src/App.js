92126d4f1b1a32e739d898e5541f1f7d88e8383b
Murial Anindo <murial.anindo@lonett.com>
2022-06-24 12:23:17 -0400

    added images and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+// import Layout from "./components/Layout";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import Blog from "./pages/blog/index";
+import Portfolio from "./pages/portfolio/index";
 import Layout from "./components/Layout";
 import "./style.css";
+
+const AppWrapper = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+    },
+
+    { path: "/Blog", element: <Blog /> },
+    { path: "/Portfolio", element: <Portfolio /> },
+  ]);
+  return routes;
+};
 
 function App() {
   return (
     <div className="container">
       <Layout />
+      <Router>
+        <AppWrapper />
+      </Router>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import "../styles/index.css";
 function Nav() {
   return (
     <div className="Navbar">
-      <h3>Murial's React Journey</h3>
+      <h3>Murial's Software Development Journey</h3>
       <ul>
         <p>Portfolio</p>
         <p>Blog</p>
