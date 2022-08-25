64775bbe8b010c07eae993c549fc5b41ed9cad81
Murial Anindo <murial.anindo@lonett.com>
2022-08-25 12:35:03 -0400

    removing tailwind and just using react and boostrap

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,24 @@
+import "./TopBar.css";
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i class="fa-brands fa-linkedin"></i>
+
+        <i class="fa-brands fa-github"></i>
+        <i class="fa-brands fa-medium"></i>
+      </div>
+      <div className="topRight">
+        <img src="" alt="" />
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
